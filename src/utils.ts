c3147d6b3ea0026bf7bd0f505e63c525ee08cc7e
Evren Ceyhan <evren@ceyhan.io>
2022-04-21 16:29:35 +0300

    obsolete utils removed

--- src/utils.ts
+++ src/utils.ts
@@ -16,8 +16,8 @@ export const calculateFactor = (x: number, y = 256) =>
 
 /**
  * Convert a Uint8Array to a hex string.
- * 
- * Same as below: 
+ *
+ * Same as below:
  *  const buffer = Buffer.from('0x010203040506', 'hex');
  */
 export const uint8ToHex = (uint8: Uint8Array): Hex =>
@@ -28,7 +28,7 @@ export const uint8ToHex = (uint8: Uint8Array): Hex =>
 
 /**
  * Convert a hex string to a Uint8Array.
- * 
+ *
  * Same as below:
  *  const hexString = Buffer.from(uint8array).toString('hex');
  */
@@ -42,27 +42,7 @@ export const uint8FromHex = (hex: Hex): Uint8Array | never => {
 };
 
 /**
- * Convert a number array to a Uint8Array.
- */
-export const uint8FromNumbers = (numbers: number[]): Uint8Array =>
-    Uint8Array.from(numbers);
-
-/**
- * Convert an ArrayBufferView to a Uint8Array.
- */
-export const uint8FromBufferView = (view: ArrayBufferView): Uint8Array =>
-    new Uint8Array(view.buffer, view.byteOffset, view.byteLength);
-
-/**
  * Convert a BufferLike object to a Uint8Array.
  */
-export const uint8FromBufferLike = (buffer: BufferLike): Uint8Array => {
-    // convert array of numbers to Uint8Array and return
-    if (Array.isArray(buffer)) return uint8FromNumbers(buffer);
-
-    // convert ArrayBufferView to Uint8Array and return
-    if (ArrayBuffer.isView(buffer)) return uint8FromBufferView(buffer);
-
-    // return as-is if already Uint8Array
-    return buffer;
-};
+export const uint8FromBufferLike = (buffer: BufferLike): Uint8Array =>
+    buffer instanceof Uint8Array ? buffer : new Uint8Array(buffer);
